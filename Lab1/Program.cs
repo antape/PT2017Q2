4db8b6c3bb405e38a781fc3aadd8ff45314d36c5
Anuar <anyardj@gmail.com>
2017-01-23 19:25:11 +0600

    Lab 1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,7 +10,72 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            test;
+            Student s = new Student("Anuar", "Jussupov", 21, 4.0, 2);
+            
+           
+            Console.WriteLine("Name: {0} \nSurname: {1} \nAge: {2} \nGPA: {3} \nCourse: {4}", s.name, s.surname, s.age, s.gpa, s.course);
+
+            
+            Student[] s_t = new Student[2];
+            
+            
+
+            
+
+           
+            for (int i = 0; i < s_t.Length; i++)
+            {
+                Console.WriteLine("\nStudent #{0}", i+1);
+                  
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Surname: ");
+                string surname = Console.ReadLine();
+                Console.Write("Age: ");
+                int age = int.Parse(Console.ReadLine());
+                Console.Write("GPA: ");
+                double gpa = double.Parse(Console.ReadLine());
+                Console.Write("Course: ");
+                int course = int.Parse(Console.ReadLine());
+                s_t[i] = new Student(name, surname, age, gpa, course);
+                Console.WriteLine("Done... \n\n");
+            }
+
+
+
+
+            Console.WriteLine("\n Database"); 
+
+            for (int i = 0; i < s_t.Length; i++)
+            {
+                Console.WriteLine("\n\nName: {0} \nSurname: {1} \nAge: {2} \nGPA: {3} \nCourse: {4}", s_t[i].name, s_t[i].surname, s_t[i].age, s_t[i].gpa, s_t[i].course);
+                
+                   
+            }
+            
+
+
+            Console.ReadKey();
         }
     }
+
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public double gpa;
+        public int course; 
+
+        public Student(string _name, string _surname, int _age, double _gpa, int _course)
+        {
+            name = _name;
+            surname = _surname;
+            age = _age;
+            gpa = _gpa;
+            course = _course;  
+        }
+
+    }
+
 }
