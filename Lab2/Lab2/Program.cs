af9a01e55877d71c740e5c8840f4bebc8b25486a
Anuar KBTU <kbtu@kbtu.kz>
2017-01-24 13:40:48 +0600

    Week2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -6,11 +6,46 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
+    class Complex
+    {
+        public int a, b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+                    }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex tmp = new Complex(0,0);
+
+            tmp.a = c1.a + c2.a;
+            tmp.b = c1.b + c2.b;
+
+            return tmp;
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}i", a, b);
+        }
+    }
+
+
+    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            Complex c1 = new Complex(2, 4);
+            Complex c2 = new Complex(2, 9); 
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+
+            Console.WriteLine(c1 + c2);
+
+            Console.ReadKey();
         }
     }
 }
