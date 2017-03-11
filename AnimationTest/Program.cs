2c56a906a550a676314451b0cd446f5a6df4b578
anyap <anyap@LAPTOP-62UL42RO>
2017-03-11 19:17:16 +0600

    tests

---
+++ AnimationTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false; // гасим курсор
+            ConsoleKeyInfo k;
+
+            // Координаты игрока
+            int playerX = 15;
+            int playerY = 15;
+            char playerChar = 'X';
+
+            // Координаты флага
+            int flagX = 25;
+            int flagY = 5;
+
+            do
+            {
+                Console.Clear();
+
+                Console.SetCursorPosition(flagX, flagY);
+                Console.Write("F"); // флаг
+
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write(playerChar);
+                // Анимация движения игрока
+               // playerChar = playerChar == 'X' ? 'Y' : 'X';
+
+                k = Console.ReadKey(true);
+
+                if (k.Key == ConsoleKey.UpArrow)
+                    playerY--;
+                else if (k.Key == ConsoleKey.DownArrow)
+                    playerY++;
+                else if (k.Key == ConsoleKey.LeftArrow)
+                    playerX--;
+                else if (k.Key == ConsoleKey.RightArrow)
+                    playerX++;
+
+                if (playerX == flagX && playerY == flagY)
+                {
+                    Console.Clear();
+                    Console.WriteLine("EPIC WIN!");
+                    break; // выходим из цикла do - while
+                }
+
+            } while (k.Key != ConsoleKey.Escape); // выходим из цикла по нажатию Esc
+
+            Console.CursorVisible = true;
+        }
+    }
+}
---
+++ AnimationTest2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadExample
+{
+    public class Worker
+    {
+        public bool isActive = true;
+        public int x = 10;
+        public int y = 10;
+        public int dx = 0;
+        public int dy = 0;
+        public int timer = 1000;
+        public void work()
+        {
+            while (isActive)
+            {
+                Thread.Sleep(timer);
+                Console.CursorVisible = false;
+                Console.Clear();
+                x += dx;
+                y += dy;
+                Console.SetCursorPosition(x, y);
+                Console.Write('*');
+            }
+        }
+        public void stop()
+        {
+            isActive = false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker w = new Worker();
+            Thread t = new Thread(new ThreadStart(w.work));
+            t.Start();
+            while (w.isActive)
+            {
+                ConsoleKeyInfo k = Console.ReadKey();
+                if (k.Key == ConsoleKey.UpArrow)
+                {
+                    w.dx = 0;
+                    w.dy = -1;
+                }
+                else if (k.Key == ConsoleKey.DownArrow)
+                {
+                    w.dx = 0;
+                    w.dy = 1;
+                }
+                else if (k.Key == ConsoleKey.RightArrow)
+                {
+                    w.dx = 1;
+                    w.dy = 0;
+                }
+                else if (k.Key == ConsoleKey.LeftArrow)
+                {
+                    w.dx = -1;
+                    w.dy = 0;
+                }
+                else if (k.Key == ConsoleKey.S)
+                {
+                    w.timer = (int)(w.timer * 0.7);
+                }
+                else if (k.Key == ConsoleKey.Escape) w.stop();
+            }
+            Console.WriteLine();
+            Console.WriteLine("The end");
+            Console.ReadKey();
+        }
+    }
+}
